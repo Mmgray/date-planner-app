a3bc3efe76e1209984defc88f8273147d55f127c
Misti Gray <misti.mechelle@gmail.com>
2018-07-07 12:37:00 -0500

    merge

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 //import React from 'react';
 //import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
 import App from './App';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-=======
-
-ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> e86c20a1422a513c6356df58606fdd27a58aa9db
