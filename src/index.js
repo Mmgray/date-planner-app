cb3b7efe4a900359dee5943c93f8ede1daaaea94
Sidney Offord <sidneyofford@gmail.com>
2018-07-09 18:50:56 -0500

    new map

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import App from './App';
-// // import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-=======
 
 ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> e86c20a1422a513c6356df58606fdd27a58aa9db
-=======
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> 2c907a67daf005926983f22f38e5aea050fa9b2f
