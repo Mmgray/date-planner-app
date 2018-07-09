33e74867dbda6551e41fb10f1b61534bc7d7feda
amber phillips <amber.ranae2@gmail.com>
2018-07-09 14:00:38 -0500

    fixed app.js page

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,33 +1,3 @@
 import React from "react";
-import "./Search.css";
+import "./SearchForm.css";
 
-const SearchForm = props => (
-    <form className="search">
-      <div className="form-group">
-        <label htmlFor="Date">Search for Restaurants,Entertainment:</label>
-        <input
-          value={props.search}
-          onChange={props.handleInputChange}
-          name="breed"
-          list="breeds"
-          type="text"
-          className="form-control"
-          placeholder="Type in a dog breed to begin"
-          id="breed"
-        />
-        <datalist id="breeds">
-          {props.breeds.map(breed => <option value={breed} key={breed} />)}
-        </datalist>
-        <button
-          type="submit"
-          onClick={props.handleFormSubmit}
-          className="btn btn-success"
-        >
-          Search
-        </button>
-      </div>
-    </form>
-  );
-  
-  export default SearchForm;
-  
