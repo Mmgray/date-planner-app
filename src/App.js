7b022a27a1685acc956f39b163dc349ab3f1587f
aaronmcma49 <aaronmcmanus49@gmail.com>
2018-07-09 21:04:57 -0500

    adding jumbotron

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, {Component} from "react";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import SearchForm from "./components/SearchForm";
-import SearchResults from "./components/SearchResults";
-import Footer from "./components/Footer";
-import Carousel from "./components/Carousel";
-import Wrapper from "./components/Wrapper";
+// import Navbar from "./components/Navbar";
+// import SearchForm from "./components/SearchForm";
+// import SearchResults from "./components/SearchResults";
+// import Footer from "./components/Footer";
+// import Carousel from "./components/Carousel";
+// import Wrapper from "./components/Wrapper";
 import Home from "./pages/Home";
 import datepg from "./pages/datepg";
-import signup from "./pages/signup";
+// import signup from "./pages/signup";
 import Navbar from "./components/Navbar/Navbar";
+import Jumbotron from "./components/Jumbotron/jumbotron";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
-        <h1>Let Us Help You Plan Your Next Date!</h1>          
+        <div>        
           <Navbar />
           <Switch>
             <BrowserRouter exact path="/" component={Home} />
             <BrowserRouter exact path="/datepg" component={datepg} />
-            <BrowserRouter exact path="/signup" component={signup} />
+            {/* <BrowserRouter exact path="/signup" component={signup} /> */}
           </Switch>
           <div className ="content">
          
           </div>
+          <Jumbotron />
         </div>
       </BrowserRouter>
     )
---
+++ src/components/Jumbotron/jumbotron.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import $ from 'jquery';
+
+
+class Jumbotron extends Component {
+
+    componentDidMount() {
+        $(document).ready(function () {
+            var header = $('body');
+
+            var backgrounds = new Array(
+                ['url(https://grancaffelaquila.com/wp-content/uploads/2015/11/ALEX-9-2-Group-Dining-_mini.png)'
+                , 'url(https://cdn.shopify.com/s/files/1/0759/5747/files/DSC_5024w.jpg?v=1517529850)']
+            );
+
+            var current = 0;
+
+            function nextBackground() {
+                current++;
+                current = current % backgrounds.length;
+                header.css('background-image', backgrounds[current]);
+            }
+            setInterval(nextBackground, 10000);
+
+            header.css('background-image', backgrounds[0]);
+        });
+    }
+
+    render() {
+        return (
+
+            <div className="container" >
+
+                <div className="jumbotron" >
+                    <div className="searchbar">
+                        <form action="/action_page.php">
+                           
+                     <h1 className="display-3">Hello, world!</h1>
+                    <hr className="my-4" />
+                    <p className="lead" />
+                     
+                        <a className="btn btn-primary btn-lg" href="" role="button">Learn more</a>
+                        <input type="text" placeholder="Search.." name="search" id="searchy" />
+                           <button type="submit"><i className="fa fa-search"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Jumbotron;
