7b379f9ab44fe3aaec6c6c215edc304a1c4fb36b
amber phillips <amber.ranae2@gmail.com>
2018-07-11 18:49:40 -0500

    added yarn lock/commented carousel

--- src/components/CarouselSlider/CarouselSlider.js
+++ src/components/CarouselSlider/CarouselSlider.js
@@ -1,32 +1,32 @@
-import "./CarouselSlider.css";
-import React, {Component} from 'react'
-import CarouselSlider from 'react-carousel-slider';
-let textBoxStyle = {
-    width: "50%",
-    background: "transparent",
-    textAlign: "right",
-    color: "black"
-};
+// import "./CarouselSlider.css";
+// import React, {Component} from 'react'
+// import CarouselSlider from 'react-carousel-slider';
+// let textBoxStyle = {
+//     width: "50%",
+//     background: "transparent",
+//     textAlign: "right",
+//     color: "black"
+// };
 
 
 
 
-class App extends Component {
-    render() {
+// class App extends Component {
+//     render() {
     
-        let data = [
-            {
-                des: "1",
-                imgSrc: "https://i.imqur.com/yourImage.jpg"
-            },
-            {
-                des: "2",
-                imgSrc: "https://i.imqur.com/yourImage2.jpg"
-            }
-        ];
+//         let data = [
+//             {
+//                 des: "1",
+//                 imgSrc: "https://i.imqur.com/yourImage.jpg"
+//             },
+//             {
+//                 des: "2",
+//                 imgSrc: "https://i.imqur.com/yourImage2.jpg"
+//             }
+//         ];
         
-        return <CarouselSlider slideItems = {data} />;
-    }
-}
+//         return <CarouselSlider slideItems = {data} />;
+//     }
+// }
 
-export default App;
+// export default App;
